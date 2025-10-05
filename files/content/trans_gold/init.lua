234927205aa0dfedaac9df8593ca7a5e0502f83d
deathhancox <deathhancox@gmail.com>
2025-10-05 10:53:51 -0400

    Finalization of initial version

--- files/content/trans_gold/init.lua
+++ files/content/trans_gold/init.lua
@@ -3,6 +3,8 @@ local nxml = dofile_once("mods/trans_gold/files/lib/nxml/nxml.lua")
 
 ModMaterialsFileAdd("mods/trans_gold/files/content/trans_gold/materials.xml")
 
+
+-- Gold Nugget XML Changes
 for content in nxml.edit_file("data/entities/items/pickup/goldnugget.xml") do
 	content:add_child(nxml.new_element("SpriteComponent", {
 		_tags = "enabled_in_world",
@@ -42,10 +44,42 @@ end
 for content in nxml.edit_file("data/entities/items/pickup/goldnugget_1000.xml") do
 	content:add_child(nxml.new_element("SpriteComponent", {
 		_tags = "enabled_in_world",
-		offset_x="10",
-		offset_y="10",
 		image_file = "mods/trans_gold/files/content/trans_gold/transnugget_20px.xml",
 	}))
 
 	content:first_of("PhysicsImageShapeComponent"):set("material", "gold_invisible_box2d")
+end
+
+
+-- Blood Nugget XML Chanegs
+for content in nxml.edit_file("data/entities/items/pickup/bloodmoney_10.xml") do
+	content:first_of("Base"):add_child(nxml.new_element("SpriteComponent", {
+		_remove_from_base="1",
+		_tags = "enabled_in_world",
+		image_file=""
+	}))
+end
+
+for content in nxml.edit_file("data/entities/items/pickup/bloodmoney_50.xml") do
+	content:first_of("Base"):add_child(nxml.new_element("SpriteComponent", {
+		_remove_from_base="1",
+		_tags = "enabled_in_world",
+		image_file=""
+	}))
+end
+
+for content in nxml.edit_file("data/entities/items/pickup/bloodmoney_200.xml") do
+	content:first_of("Base"):add_child(nxml.new_element("SpriteComponent", {
+		_remove_from_base="1",
+		_tags = "enabled_in_world",
+		image_file=""
+	}))
+end
+
+for content in nxml.edit_file("data/entities/items/pickup/bloodmoney_1000.xml") do
+	content:first_of("Base"):add_child(nxml.new_element("SpriteComponent", {
+		_remove_from_base="1",
+		_tags = "enabled_in_world",
+		image_file=""
+	}))
 end
