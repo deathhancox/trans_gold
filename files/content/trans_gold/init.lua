44ad1e12af5c03107efdb444c8cc044fd93ed412
deathhancox <deathhancox@gmail.com>
2025-10-05 09:16:33 -0400

    Updated to use xml and corrected xml sprite data for offsets. (Sprites currently bugged)

--- files/content/trans_gold/init.lua
+++ files/content/trans_gold/init.lua
@@ -6,46 +6,46 @@ ModMaterialsFileAdd("mods/trans_gold/files/content/trans_gold/materials.xml")
 for content in nxml.edit_file("data/entities/items/pickup/goldnugget.xml") do
 	content:add_child(nxml.new_element("SpriteComponent", {
 		_tags = "enabled_in_world",
-		image_file = "mods/trans_gold/files/content/trans_gold/sprites/transnugget_01.png",
+		image_file = "mods/trans_gold/files/content/trans_gold/transnugget_01.xml",
 	}))
 
-	-- content:first_of("PhysicsImageShapeComponent"):set("material", "gold_invisible_box2d")
+	content:first_of("PhysicsImageShapeComponent"):set("material", "gold_invisible_box2d")
 end
 
 for content in nxml.edit_file("data/entities/items/pickup/goldnugget_10.xml") do
 	content:add_child(nxml.new_element("SpriteComponent", {
 		_tags = "enabled_in_world",
-		image_file = "mods/trans_gold/files/content/trans_gold/sprites/transnugget_6px.png",
+		image_file = "mods/trans_gold/files/content/trans_gold/transnugget_6px.xml",
 	}))
 
-	-- content:first_of("PhysicsImageShapeComponent"):set("material", "gold_invisible_box2d")
+	content:first_of("PhysicsImageShapeComponent"):set("material", "gold_invisible_box2d")
 end
 
 for content in nxml.edit_file("data/entities/items/pickup/goldnugget_50.xml") do
 	content:add_child(nxml.new_element("SpriteComponent", {
 		_tags = "enabled_in_world",
-		image_file = "mods/trans_gold/files/content/trans_gold/sprites/transnugget_9px.png",
+		image_file = "mods/trans_gold/files/content/trans_gold/transnugget_9px.xml",
 	}))
 
-	-- content:first_of("PhysicsImageShapeComponent"):set("material", "gold_invisible_box2d")
+	content:first_of("PhysicsImageShapeComponent"):set("material", "gold_invisible_box2d")
 end
 
 for content in nxml.edit_file("data/entities/items/pickup/goldnugget_200.xml") do
 	content:add_child(nxml.new_element("SpriteComponent", {
 		_tags = "enabled_in_world",
-		image_file = "mods/trans_gold/files/content/trans_gold/sprites/transnugget_12px.png",
+		image_file = "mods/trans_gold/files/content/trans_gold/transnugget_12px.xml",
 	}))
 
-	-- content:first_of("PhysicsImageShapeComponent"):set("material", "gold_invisible_box2d")
+	content:first_of("PhysicsImageShapeComponent"):set("material", "gold_invisible_box2d")
 end
 
 for content in nxml.edit_file("data/entities/items/pickup/goldnugget_1000.xml") do
 	content:add_child(nxml.new_element("SpriteComponent", {
 		_tags = "enabled_in_world",
-		offset_x = "0",
-		offset_y = "0",
-		image_file = "mods/trans_gold/files/content/trans_gold/sprites/transnugget_20px.png",
+		offset_x="10",
+		offset_y="10",
+		image_file = "mods/trans_gold/files/content/trans_gold/transnugget_20px.xml",
 	}))
 
-	-- content:first_of("PhysicsImageShapeComponent"):set("material", "gold_invisible_box2d")
+	content:first_of("PhysicsImageShapeComponent"):set("material", "gold_invisible_box2d")
 end
